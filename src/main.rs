12a2bfd92b48fc49fb89eff07b79648176950e61
ByteSudoer <ByteSudoer@github.com>
2024-02-17 13:18:05 +0100

    Added Log dependencies and test tracing messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+fn init() {
+    let env_key = "RUST_LOG";
+    env::set_var(env_key, "debug");
+    tracing_subscriber::fmt::init();
+}
+
 fn main() {
-    println!("Hello, world!");
+    init();
+    tracing::info!("INFO");
+    tracing::debug!("DEBUF");
+    tracing::error!("ERROR");
 }
